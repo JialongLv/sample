051f0fcbdd58e6833379d22e3d4b677ee368702b
Jialong <ljl9410@126.com>
2018-05-29 01:55:02 +0000

    Finsh login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -32,6 +32,7 @@ class UsersController extends Controller
            'password' => bcrypt($request->password),
         ]);
 
+        Auth::login($user);
         session()->flash('success','欢迎，你将在这里开创一段新的里程');
         return redirect()->route('users.show',[$user]);
     }
